077512adf82c5ac12db7375f78df682fe0f0c28c
Kai3234 <khai2892005@gmail.com>
2025-01-18 21:02:54 +0700

    supabase

--- reflex/src/App.jsx
+++ reflex/src/App.jsx
@@ -2,32 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { createClient } from "@supabase/supabase-js";
+
+  const supabase = createClient("https://ixlinelhdftvgzajqafx.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml4bGluZWxoZGZ0dmd6YWpxYWZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY5NTYxNDEsImV4cCI6MjA1MjUzMjE0MX0.szQj9eOwjlUF46EaWC18wJhiHueNcQjHP44gsn37H34");
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+        
     </>
   )
 }
