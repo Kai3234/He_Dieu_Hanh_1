af6919052922a7551b8dbb7323fd6e2f499e3cdd
Kai3234 <khai2892005@gmail.com>
2025-01-21 22:51:09 +0700

    supabase

--- reflex/src/App.jsx
+++ reflex/src/App.jsx
@@ -2,9 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { createClient } from "@supabase/supabase-js";
-
-  const supabase = createClient("https://ixlinelhdftvgzajqafx.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml4bGluZWxoZGZ0dmd6YWpxYWZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY5NTYxNDEsImV4cCI6MjA1MjUzMjE0MX0.szQj9eOwjlUF46EaWC18wJhiHueNcQjHP44gsn37H34");
+import { supabase } from './supabaseClient'
 
 
 function App() {
