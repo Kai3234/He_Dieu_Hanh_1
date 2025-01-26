e328a27edd8c6130aa87b598ccf00f37107eba3c
Kai3234 <khai2892005@gmail.com>
2025-01-26 19:45:04 +0700

    add button

--- reflex/src/App.jsx
+++ reflex/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { supabase } from './supabaseClient'
+import { Button } from './components/ui/button'
 
 
 function App() {
@@ -10,7 +11,6 @@ function App() {
 
   return (
     <>
-      
         
     </>
   )
